c254a0b29ee5ca169f8a274675c08f81915afd2a
timelessover <shanpaodaxia@qq.com>
2018-08-05 16:51:39 +0800

    3

--- index.js
+++ index.js
@@ -1,61 +1,95 @@
 var yyy = document.getElementById('canvas')
 var ctx = yyy.getContext('2d');
 autoSetCanvasSize(yyy)
-linstenToMouse(yyy)
+listenTOUser(yyy)
+function drawCircle(x, y, radius) {
+    ctx.beginPath()
+    ctx.fillStyle = 'black'
+    ctx.arc(x, y, radius, 0, Math.PI * 2)
+    ctx.fill()
+}
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.strokeStyle = 'black'
+    ctx.moveTo(x1, y1)
+    ctx.lineWidth = 3
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+    ctx.closePath()
+}
 var eraserEnabled = false
 /********/ 
-function linstenToMouse(canvas){
+function listenTOUser(canvas){
     var using = false
     var lastPoint = { x: undefined, y: undefined }
-    //按下去鼠标
-    canvas.onmousedown = function (a) {
-
-        var x = a.clientX
-        var y = a.clientY
-        using = true
-        if (eraserEnabled) {
-            ctx.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-            lastPoint = { "x": x, "y": y }
-        }
-
-    }
-    //动鼠标
-    canvas.onmousemove = function (a) {
-        var x = a.clientX
-        var y = a.clientY
-        if(!using){return}
-        if (eraserEnabled) {
-            
+    //特性检测
+    if(document.body.ontouchstart !== undefined){
+        canvas.ontouchstart = function(a){
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            console.log(x,y)
+            using = true
+            if (eraserEnabled) {
                 ctx.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-           
+            } else {
+                lastPoint = { "x": x, "y": y }
+            }
+        }
+        canvas.ontouchmove = function(a){
+            var x = a.touches[0].clientX
+            var y = a.touches[0].clientY
+            if (!using) { return }
+            if (eraserEnabled) {
+
+                ctx.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+
                 var newPoint = { 'x': x, 'y': y }
                 drawCircle(x, y, 1)
                 drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
                 lastPoint = newPoint
+            }
+        }
+        canvas.ontouchend = function(){
+            using = false
+        }
+    }else{
+        //按下去鼠标
+        canvas.onmousedown = function (a) {
+
+            var x = a.clientX
+            var y = a.clientY
+            using = true
+            if (eraserEnabled) {
+                ctx.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                lastPoint = { "x": x, "y": y }
+            }
+
+        }
+        //动鼠标
+        canvas.onmousemove = function (a) {
+            var x = a.clientX
+            var y = a.clientY
+            if (!using) { return }
+            if (eraserEnabled) {
+
+                ctx.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+
+                var newPoint = { 'x': x, 'y': y }
+                drawCircle(x, y, 1)
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
+            }
+        }
+        //松开鼠标
+        canvas.onmouseup = function (aaa) {
+            using = false
         }
     }
-    //松开鼠标
-    canvas.onmouseup = function (aaa) {
-        using = false
     }
-    function drawCircle(x, y, radius) {
-        ctx.beginPath()
-        ctx.fillStyle = 'black'
-        ctx.arc(x, y, radius, 0, Math.PI * 2)
-        ctx.fill()
-    }
-    function drawLine(x1, y1, x2, y2) {
-        ctx.beginPath()
-        ctx.strokeStyle = 'black'
-        ctx.moveTo(x1, y1)
-        ctx.lineWidth = 3
-        ctx.lineTo(x2, y2)
-        ctx.stroke()
-        ctx.closePath()
-    }
-}
+    
 
 /*****/
 var eraserEnabled = false
